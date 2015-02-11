d33c7807e3e44ca1a8159f9d8c929aae39931e56
Sebastián Valencia <sc.valencia606@gmail.com>
2015-02-10 20:41:00 -0500

    Model as is in an episode

--- aura/app/models/EpisodioVoz.java
+++ aura/app/models/EpisodioVoz.java
@@ -8,14 +8,16 @@ package models;
 import play.db.ebean.Model;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.util.Date;
 
 @Entity
 public class EpisodioVoz extends Model {
 
-    public String email;
+    public Long docIdentidad;
+    public String nombre;
     public String password;
-    public String fullname;
-    public boolean isAdmin;
+    public Date fechaNacimiento;
+    
 
     public EpisodioVoz(String email, String password, String fullname) {
         this.email = email;
