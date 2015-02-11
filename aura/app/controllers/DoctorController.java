4f90326b44c995c5755a9f953e33aa80ed1a04ee
claubm777 <claubm777@hotmail.com>
2015-02-10 20:54:27 -0500

    Controller

---
+++ aura/app/controllers/DoctorController.java
@@ -0,0 +1,40 @@
+package controllers;
+
+import play.mvc.Controller;
+
+import javax.xml.transform.Result;
+
+/**
+ * Created by Clau on 10/02/2015.
+ */
+public class DoctorController extends Controller {
+    public static Result crearDoctor(String nombre){
+        return null;
+    }
+
+    public static Result eliminarDoctor(String nombre){
+        return null;
+    }
+
+    public static Result actualizarDoctor(String nombre){
+        return null;
+    }
+
+    public static Result verEpisodiosId(String nombre){
+        return null;
+    }
+
+    public static Result verEpisodiosFecha(String nombre){
+        return null;
+    }
+
+    public static Result verEpisodiosEscritos(String nombre){
+        return null;
+    }
+
+    public static Result verEpisodiosVoz(String nombre){
+        return null;
+    }
+
+
+}
