fb1ce8e6d15259c43bb07e4fb08a7e8a58725f9c
Sebastián Valencia <sc.valencia606@gmail.com>
2015-02-11 19:21:45 -0500

    Model update

--- aura/app/models/EpisodioEscrito.java
+++ aura/app/models/EpisodioEscrito.java
@@ -4,10 +4,42 @@ package models;
  * Created by Clau on 10/02/2015.
  */
 
+import play.data.validation.Constraints;
 import play.db.ebean.Model;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.util.Date;
 
 @Entity
 public class EpisodioEscrito extends Model {
+
+    public static final int INTENSIDAD_NIVEL1 = 1;
+    public static final int INTENSIDAD_NIVEL2 = 2;
+    public static final int INTENSIDAD_NIVEL3 = 3;
+    public static final int INTENSIDAD_NIVEL4 = 4;
+    public static final int INTENSIDAD_NIVEL5 = 5;
+
+    public static final int LUGAR_OSCURO = 1;
+    public static final int LUGAR_ABIERTO_SOLEADO = 2;
+    public static final int LUGAR_RUIDOSO = 3;
+    public static final int LUGAR_CON_OLORES_FUERTES= 4;
+
+
+    @Id
+    public Long id;
+
+    @Constraints.Required
+    public Date fechaPublicacion;
+
+    @Constraints.Required
+    public int intensidad;
+
+
+    public Double horasSuenio;
+
+    public boolean suenioRegular;
+
+    public int lugar;
+
+    public boolean episodioEstreCercano;
 }
