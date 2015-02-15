1230802ff7f29532a20c2e053f249464cc461e84
Sebastián Valencia <sc.valencia606@gmail.com>
2015-02-15 14:03:28 -0500

    Models, services, routes with HTTP definition and JPA integration

--- aura/app/models/Alimento.java
+++ aura/app/models/Alimento.java
@@ -4,17 +4,22 @@ package models;
  * Created by Clau on 10/02/2015.
  */
 
-import play.db.ebean.Model;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Alimento extends Model
-{
+public class Alimento {
     @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     public Long id;
     public String nombre;
     public Integer cantidad;
+
+    public Alimento() {
+
+    }
 
     public Alimento (String nombre, Integer cantidad){
         this.nombre=nombre;
