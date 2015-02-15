4ebd2ba10c36e07f58209eb3ca6725a58466bf61
Sebastián Valencia <sc.valencia606@gmail.com>
2015-02-15 14:04:00 -0500

    Models, services, routes with HTTP definition and JPA integration

--- aura/app/models/Paciente.java
+++ aura/app/models/Paciente.java
@@ -4,53 +4,92 @@ package models;
  * Created by Clau on 10/02/2015.
  */
 
-
-import play.db.ebean.*;
-
 import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.mindrot.jbcrypt.BCrypt;
+import play.db.jpa.*;
 import java.util.Date;
 import java.util.List;
+
 @Entity
-public class Paciente extends Model {
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Paciente {
 
     public static final int MASCULINO = 1;
     public static final int FEMENINO = 1;
 
     @Id
-    public Long docIdentidad;
-    public String nombre;
-    public String password;
-    public Date fechaNacimiento;
-    public String email;
-    public int genero;
-    public static Finder<Long,Paciente> find = new Finder(
-            Long.class, Paciente.class
-    );
+    private Long docIdentidad;
+    private String nombre;
+    private String password;
+    private Date fechaNacimiento;
+    private String email;
 
-    public static List<Paciente> all() {
-        return find.all();
+    public int getGenero() {
+
+        return genero;
     }
 
-    public static void create(Paciente paciente) {
-        paciente.save();
+    public void setGenero(int genero) {
+        this.genero = genero;
     }
 
-    public static void delete(Long id) {
-        find.ref(id).delete();
-    }
-    public static Paciente buscarUnoId(Long id){
-       return find.byId(id);
+    public Long getDocIdentidad() {
+        return docIdentidad;
     }
 
+    public void setDocIdentidad(Long docIdentidad) {
+        this.docIdentidad = docIdentidad;
+    }
 
+    public String getNombre() {
+        return nombre;
+    }
 
-    public Paciente(String email, String password, Long docIdentidad,String nombre,Date fechaNacimiento, int genero) {
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password = BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
         this.email = email;
-        this.password = password;
-        this.docIdentidad=docIdentidad;
-        this.nombre=nombre;
-        this.fechaNacimiento=fechaNacimiento;
-        this.genero=genero;
+    }
+
+    private int genero;
+
+    public Paciente() {
+
+    }
+
+    public static Paciente create(String email, String password, Long docIdentidad,String nombre,Date fechaNacimiento, int genero) {
+        // http://stackoverflow.com/questions/15657062/play-framework-2-best-way-to-store-password-hash-of-user
+        Paciente p = new Paciente();
+        p.docIdentidad = docIdentidad;
+        p.nombre = nombre;
+        p.password = BCrypt.hashpw(password, BCrypt.gensalt());
+        p.fechaNacimiento = fechaNacimiento;
+        p.email = email;
+        return p;
     }
 
 }
