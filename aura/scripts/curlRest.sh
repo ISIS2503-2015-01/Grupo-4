f8bf8a30854d1cae977d0dadd1dfed9221975094
Sebastián Valencia <sc.valencia606@gmail.com>
2015-02-16 11:45:26 -0500

    Some cools scripts for DB population and CURL via testing

--- aura/scripts/curlRest.sh
+++ aura/scripts/curlRest.sh
@@ -1,5 +1,3 @@
-curl -i -H "Accept: application/json" -X DELETE http://localhost:9000/api/paciente/10
-
 curl -v -H "Content-type: application/json" -X PUT -d '{"email":"sc@sada.com", "docIdentidad" : "10", "nombre" : "Camilo", "fechaN" : "2000-09-09", "genero" : "1", "password" : "zadvczzvz"}' http://localhost:9000/api/paciente
 
 curl -v -H "Content-type: application/json" -X POST -d '{"URL" : "0", "intensidad" : "1", "horasSuenio" : "3", "regularidad" : "true", "localizacion" : "4", "estres" : "false"}' http://localhost:9000/api/paciente/10/episodio
