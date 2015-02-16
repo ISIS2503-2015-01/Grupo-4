6911d88f81a947f9b71f306da8e19eef91ba6d3d
Sebastián Valencia <sc.valencia606@gmail.com>
2015-02-16 12:32:40 -0500

    CRUP for patients and episodes

--- aura/scripts/curlRest.sh
+++ aura/scripts/curlRest.sh
@@ -1,4 +1,2 @@
-curl -v -H "Content-type: application/json" -X PUT -d '{"email":"sc@sada.com", "docIdentidad" : "10", "nombre" : "Camilo", "fechaN" : "2000-09-09", "genero" : "1", "password" : "zadvczzvz"}' http://localhost:9000/api/paciente
-
-curl -v -H "Content-type: application/json" -X POST -d '{"URL" : "0", "intensidad" : "1", "horasSuenio" : "3", "regularidad" : "true", "localizacion" : "4", "estres" : "false"}' http://localhost:9000/api/paciente/10/episodio
+url -v -H "Content-type: application/json" -X POST -d '{"URL" : "0", "intensidad" : "1", "horasSuenio" : "3", "regularidad" : "true", "localizacion" : "4", "estres" : "false"}' http://localhost:9000/api/paciente/10/episodio
 
