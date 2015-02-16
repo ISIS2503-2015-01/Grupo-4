64c0be3d2cef817330d41e3f23bf1dc7b132ae61
Sebastián Valencia <sc.valencia606@gmail.com>
2015-02-16 08:10:48 -0500

    Automated POJO generation and pr-JPA integration associations among classes

--- aura/app/models/Medicamento.java
+++ aura/app/models/Medicamento.java
@@ -13,5 +13,58 @@ public class Medicamento {
 
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
-    public Long id;
+    private Long id;
+
+    private String nombre;
+
+    private String dosis;
+
+    private int horasTomadoAntes;
+
+    private int episodioId;
+
+    public Medicamento() { }
+
+    public Medicamento(String nombre, String dosis, int horasTomadoAntes, int episodioId) {
+        this.nombre = nombre;
+        this.dosis = dosis;
+        this.horasTomadoAntes = horasTomadoAntes;
+        this.episodioId = episodioId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDosis() {
+        return dosis;
+    }
+
+    public void setDosis(String dosis) {
+        this.dosis = dosis;
+    }
+
+    public int getHorasTomadoAntes() {
+        return horasTomadoAntes;
+    }
+
+    public void setHorasTomadoAntes(int horasTomadoAntes) {
+        this.horasTomadoAntes = horasTomadoAntes;
+    }
+
+    public int getEpisodioId() {
+        return episodioId;
+    }
+
+    public void setEpisodioId(int episodioId) {
+        this.episodioId = episodioId;
+    }
 }
