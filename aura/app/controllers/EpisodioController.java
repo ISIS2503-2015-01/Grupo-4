fba51b6063377432debe635c559d76f0d3b40ac0
Sebastián Valencia <sc.valencia606@gmail.com>
2015-02-16 08:47:50 -0500

    Automated POJO generation and pr-JPA integration associations among classes

--- aura/app/controllers/EpisodioController.java
+++ aura/app/controllers/EpisodioController.java
@@ -3,6 +3,7 @@ package controllers;
 import com.fasterxml.jackson.databind.JsonNode;
 import models.Paciente;
 import play.db.jpa.JPA;
+import play.db.jpa.Transactional;
 import play.mvc.BodyParser;
 import play.mvc.Controller;
 import play.mvc.Result;
@@ -14,8 +15,79 @@ import java.util.Date;
 /**
  * Created by Clau on 10/02/2015.
  */
-public class EpisodioController extends Controller
-{
+public class EpisodioController extends Controller {
+
+    // CRUD
+    @Transactional
+    @BodyParser.Of(BodyParser.Json.class)
+    public static Result create(Long idPaciente) {
+
+        e.horasSuenio = horasSuenio;
+        e.suenioRegular = suenioRegular;
+        e.lugar = lugar;
+        e.episodioEstreCercano = episodioEstreCercano;
+        e.pacienteID = pacienteID;
+
+        JsonNode j = Controller.request().body().asJson();
+
+        Long idUrl = j.findPath("URL").asLong();
+        Date fechaPublicacion = new Date();
+        int intensidad = j.findPath("intensidad").asInt();
+        int horasSuenio = j.findPath("horasSuenio").asInt();
+        boolean regularidad = j.findPath("regular").asBoolean();
+
+
+
+
+        String email=j.findPath("email").asText();
+        String password = j.findPath("password").asText();
+        Long docIdentidad = Long.parseLong(j.findPath("docIdentidad").asText());
+        String name = j.findPath("nombre").asText();
+        String fechaN = j.findPath("fechaN").asText();
+        Integer genero = Integer.parseInt(j.findPath("genero").asText());
+
+
+        SimpleDateFormat formatoDelTexto = new SimpleDateFormat("yyyy-MM-dd");
+
+        Date fecha = null;
+
+
+        try {
+            fecha = formatoDelTexto.parse(fechaN);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        try {
+
+            Paciente p = Paciente.create(email, password, docIdentidad, name, fecha, genero);
+            JPA.em().persist(p);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Results.ok("Error al crear el paciente");
+        }
+
+        return Results.created();
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     public static Result reportarAlarma(Long idPaciente){
         return null;
     }
