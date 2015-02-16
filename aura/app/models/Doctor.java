ce7285ee58c2e0ae28479fe7d42501860184dd5c
Sebastián Valencia <sc.valencia606@gmail.com>
2015-02-16 08:10:20 -0500

    Automated POJO generation and pr-JPA integration associations among classes

--- aura/app/models/Doctor.java
+++ aura/app/models/Doctor.java
@@ -6,35 +6,36 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-public class Doctor{
-    @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
-    public static Long id;
+public class Doctor {
 
-    public static final int MASCULINO = 0;
-    public static final int FEMENINO = 1;
+    private static final int MASCULINO = 0;
+
+    private static final int FEMENINO = 1;
 
     //Especialidades
-    public static final int CARDIOLOGO = 0;
+    private static final int CARDIOLOGO = 0;
 
+    @Id
     private Long docIdentidad;
+
     private String nombre;
+
     private String email;
+
     private String password;
+
     private Date fechaNacimiento;
+
     private int genero;
+
     private int especialidad;
 
+    public Doctor() { }
 
-    public Doctor()
-    {
-
-    }
     public static Doctor create(String emailP, String passwordP, Long docIdentidadP,String nombreP, Date fechaNacimientoP, int generoP, int especialidadP)
     {
         // http://stackoverflow.com/questions/15657062/play-framework-2-best-way-to-store-password-hash-of-user
         Doctor d  = new Doctor();
-        d.id = id;
         d.docIdentidad = docIdentidadP;
         d.nombre = nombreP;
         d.password = BCrypt.hashpw(passwordP, BCrypt.gensalt());
@@ -55,6 +56,7 @@ public class Doctor{
     public String getNombre() {
         return nombre;
     }
+
     public void setNombre(String nombreP) {
         this.nombre = nombreP;
     }
@@ -62,6 +64,7 @@ public class Doctor{
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String emailP) {
         this.email = emailP;
     }
@@ -69,6 +72,7 @@ public class Doctor{
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String passwordP) {
         this.password = passwordP;
     }
@@ -76,6 +80,7 @@ public class Doctor{
     public Date getFechaNacimiento() {
         return fechaNacimiento;
     }
+
     public void setFechaNacimiento(Date fechaNacimientoP) {
         this.fechaNacimiento = fechaNacimientoP;
     }
@@ -83,6 +88,7 @@ public class Doctor{
     public int getGenero() {
         return genero;
     }
+
     public void setGenero(int generoP) {
         this.genero = generoP;
     }
@@ -90,6 +96,7 @@ public class Doctor{
     public int getEspecialidad() {
         return especialidad;
     }
+
     public void setEspecialidad(int especialidadP) {
         this.especialidad = especialidadP;
     }
