0c4d69b6a3e6a1e644ee46aa815b51308212dadd
Sebastián Valencia <sc.valencia606@gmail.com>
2015-02-16 08:10:06 -0500

    Automated POJO generation and pr-JPA integration associations among classes

--- aura/app/models/ActividadFisica.java
+++ aura/app/models/ActividadFisica.java
@@ -13,22 +13,81 @@ public class ActividadFisica {
 
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
-    public Long id;
-    public Integer descripcion;
-    public Integer intensidad;
-    public Integer lugar;
-    public Integer clima;
-    public Boolean hidratacion;
+    private Long id;
 
-    public ActividadFisica() {
+    private Integer descripcion;
 
-    }
+    private Integer intensidad;
 
-    public ActividadFisica(Integer descripcion, Integer intensidad,Integer lugar,Integer clima,Boolean hidratacion) {
+    private Integer lugar;
+
+    private Integer clima;
+
+    private Boolean hidratacion;
+
+    private int episodioId;
+
+    public ActividadFisica() { }
+
+    public ActividadFisica(Integer descripcion, Integer intensidad, Integer lugar, Integer clima, Boolean hidratacion, int episodioId) {
         this.descripcion = descripcion;
         this.intensidad = intensidad;
-        this.lugar=lugar;
-        this.clima=clima;
-        this.hidratacion=hidratacion;
+        this.lugar = lugar;
+        this.clima = clima;
+        this.hidratacion = hidratacion;
+        this.episodioId = episodioId;
+    }
+
+    public Long getId() {
+
+        return id;
+    }
+
+    public int getEpisodioId() {
+        return episodioId;
+    }
+
+    public void setEpisodioId(int episodioId) {
+        this.episodioId = episodioId;
+    }
+
+    public Integer getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(Integer descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Integer getIntensidad() {
+        return intensidad;
+    }
+
+    public void setIntensidad(Integer intensidad) {
+        this.intensidad = intensidad;
+    }
+
+    public Integer getLugar() {
+        return lugar;
+    }
+
+    public void setLugar(Integer lugar) {
+        this.lugar = lugar;
+    }
+
+    public Integer getClima() {
+        return clima;
+    }
+
+    public void setClima(Integer clima) {
+        this.clima = clima;
+    }
+
+    public Boolean getHidratacion() {
+        return hidratacion;
+    }
+
+    public void setHidratacion(Boolean hidratacion) {
+        this.hidratacion = hidratacion;
     }
 }
