ca5a721e7eadcb7fef7e70a6889206c5b0bf8021
cps543 <c.pinzon543@uniandes.edu.co>
2015-02-17 09:06:49 -0500

    Update DoctorController.java

--- aura/app/controllers/DoctorController.java
+++ aura/app/controllers/DoctorController.java
@@ -25,9 +25,10 @@ public class DoctorController extends Controller {
     public static Result create()
     {
         JsonNode j = Controller.request().body().asJson();
+        
+        //Para realizar pruebas de escalabilidad y desempeño, des comentar las siguientes dos líneas y comentar la tercera
         //Random random = new Random();
         //long docIdentidad = Math.abs(random.nextLong()*1000000000);
-        
         Long docIdentidad = Long.parseLong(j.findPath("docIdentidad").asText());
         String nombre = j.findPath("nombre").asText();
         String email=j.findPath("Email").asText();
