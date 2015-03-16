ad1376fe4798d77f620afb7af1a5dc31c880f262
cps543 <c.pinzon543@uniandes.edu.co>
2015-03-16 09:31:39 -0500

    Views

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -5,6 +5,7 @@ import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
+import views.html.login;
 import views.html.test;
 
 public class Application extends Controller {
@@ -21,6 +22,11 @@ public class Application extends Controller {
         jsonPuntos.put("i","3");
         String a=Json.toJson(jsonPuntos).toString();
         return ok(index.render(a));
+    }
+
+    public static Result login() {
+        return ok(
+                login.render(""));
     }
 
     public static Result test() {
